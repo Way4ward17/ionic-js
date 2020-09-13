f84ea1e111d2efbaadc32b2851f9103df5241878
way4ward17 <supersondultimate@gmail.com>
2020-09-13 23:44:06 +0100

    adding list

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const reason = document.querySelector("#input-reason");
 const amount = document.querySelector("#input-amount");
 const clear = document.querySelector("#btn-clear");
 const add = document.querySelector("#btn-add");
+const itemlist = document.querySelector("#item-list");
 
 
+add.addEventListener('click', ()=>{
+   reasonValue = reason.value;
+   amountValue = amount.value;
 
-clear.addEventListener('click', ()=>{
-    console.log("clicked");
+
+   if(reasonValue.trim().lenght <=0 || amountValue <=0 || amountValue.trim().lenght <=0){
+        return;
+   }
+const newItem = document.createElement('ion-item');
+newItem.textContent = reasonValue+' : '+amountValue;
+itemlist.appendChild(newItem);
 })
+
+
 
